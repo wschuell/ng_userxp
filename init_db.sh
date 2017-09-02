7667175411995d7ca98d8be7fa71dedc5d681218
William Schueller <william.schueller@gmail.com>
2017-09-02 12:59:40 +0200

    modified get_xp policy

--- init_db.sh
+++ init_db.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 service postgresql start
 
-su - postgres
+su - postgres << 'EOF'
 createdb ng_userxp
 psql -c "CREATE USER ng_userxp_user WITH PASSWORD 'pass';"
-
 createdb naminggames
 psql -c "CREATE USER naminggames WITH PASSWORD 'naminggames';"
-exit
+EOF
 
 DJANGO_MODULE_SETTINGS="main_site.production_settings"
 export DJANGO_MODULE_SETTINGS
-RUN python manage.py makemigrations ng && python manage.py migrate
-RUN python manage.py collectstatic
+python manage.py collectstatic
