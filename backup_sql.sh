0e1e0560156b4e8bc5f14d8351ba4446a4fb717d
William Schueller <william.schueller@gmail.com>
2017-09-06 22:11:24 +0200

    backupsql script

--- backup_sql.sh
+++ backup_sql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -t ng pg_dumpall -c -U postgres | gzip > /data/backup_postgres/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql.gz
+docker exec --user postgres -t ng pg_dumpall | gzip > /data/backup_postgres/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql.gz
