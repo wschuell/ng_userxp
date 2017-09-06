db194838e260c48ff5915de81daaf7c45d679721
William Schueller <william.schueller@gmail.com>
2017-09-07 00:40:00 +0200

    typo in changelang

--- ng/static/ng/change_lang.js
+++ ng/static/ng/change_lang.js
@@ -34,7 +34,7 @@ var lang = {"en": {"header_home": "THE NAMING GAME", "skipped_text": "Others are
         $('#home_you').html(current_lang.home_you);
         $('#window_title').html(current_lang.window_title);
         $('#img_interaction_text').html(current_lang.img_interaction_text);
-        $('#interactbutton').html(current_lang.interactonbutton);
+        $('#interactbutton').html(current_lang.interactbutton);
         //$(document).prop('title', current_lang.title);
         //$('#title').html(lang.it[0].some_id_of_a_string_div);
         //$('text#button_next').val(lang.it[0].button_next);
