502979b3a1407f521ef0e940e651d5236212c403
William Schueller <william.schueller@gmail.com>
2017-09-07 02:17:46 +0200

    changed admin side

--- ng/admin.py
+++ ng/admin.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from . import models 
 # Register your models here.
+from django_smart_autoregister import auto_configure_admin_for_model
+from django_smart_autoregister import auto_configure_admin
+
+
+models_list = [
+	models.Experiment,
+	models.XpConfig,
+	models.Meaning,
+	models.Word,
+	models.CookieId,
+	models.Score,
+	models.Role,
+	models.PastInteraction,
+]
+
+for m in models_list:
+	auto_configure_admin_for_model(m)
