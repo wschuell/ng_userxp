d0e3999a27b284b7f08925fdc9f9a8352db0d924
William Schueller <william.schueller@gmail.com>
2017-10-25 11:26:14 +0200

    added travis

--- ng/admin.py
+++ ng/admin.py
@@ -27,7 +27,7 @@ for m in models_list:
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined', 'last_login', 'is_staff')
+UserAdmin.list_display = ('username','email', 'first_name', 'last_name', 'is_active', 'date_joined', 'last_login', 'is_staff')
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
