29a00365caa7e14b8c8f749a5d6ddd642d1c84f8
William Schueller <william.schueller@gmail.com>
2017-10-25 20:19:55 +0200

    tests travis

--- init_db_test.sh
+++ init_db_test.sh
@@ -3,9 +3,9 @@
 #apt-get install --yes --force-yes postgresql postgresql-contrib libpq-dev
 
 
-psql -c "createdb ng_userxp;" -U postgres
+psql -c "CREATE DATABASE ng_userxp;" -U postgres
 psql -c "CREATE USER ng_userxp_user WITH PASSWORD 'pass';" -U postgres
-psql -c "createdb naminggames;"  -U postgres
+psql -c "CREATE DATABASE naminggames;"  -U postgres
 psql -c "CREATE USER naminggames WITH PASSWORD 'naminggames';" -U postgres
 
 DJANGO_MODULE_SETTINGS="main_site.production_settings"
