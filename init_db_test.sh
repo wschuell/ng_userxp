ac262f5aee0be7cdf5c6205c897423796e3d6d11
William Schueller <william.schueller@gmail.com>
2017-10-25 19:58:26 +0200

    tests travis

---
+++ init_db_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#apt-get install --yes --force-yes postgresql postgresql-contrib libpq-dev
+service postgresql start
+
+su - postgres << 'EOF'
+createdb ng_userxp
+psql -c "CREATE USER ng_userxp_user WITH PASSWORD 'pass';"
+createdb naminggames
+psql -c "CREATE USER naminggames WITH PASSWORD 'naminggames';"
+EOF
+
+DJANGO_MODULE_SETTINGS="main_site.production_settings"
+export DJANGO_MODULE_SETTINGS
+python manage.py makemigrations ng
+python manage.py migrate
+python manage.py collectstatic
