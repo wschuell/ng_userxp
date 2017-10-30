76b1b58d826a7d6a4746c58bc98c3b6939e763f7
William Schueller <william.schueller@gmail.com>
2017-10-30 10:10:15 +0100

    pytest

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,15 @@
 import os
+
+
+from .. import main_site
+from .. import ng
+
+
 def test_basic():
 	assert True
 
-def test_import():
-	from .. import main_site
-	from .. import ng
-
 def test_secretkey():
-	SECRET_KEY = main_site.secretkey.SECRET_KEY
+	from ..main_site import secretkey
+	SECRET_KEY = secretkey.SECRET_KEY
 	assert len(SECRET_KEY) == 50
 
