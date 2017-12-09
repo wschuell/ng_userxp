f7f6664ec9cbe3aacbd18c255e5e9eb7b02e11d7
Art2B <arthur.battut@gmail.com>
2017-12-09 16:29:53 +0100

    reindent js files and add gitignore

--- ng/static/ng/login.js
+++ ng/static/ng/login.js
@@ -5,40 +5,33 @@ $( document ).ready(function() {
     $(document).ajaxStart(function () { $("html").addClass("wait"); });
     $(document).ajaxStop(function () { $("html").removeClass("wait"); });
 
-//uuid
-function uuid()
-{
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
-}
+    //uuid
+    function uuid () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    }
 
 
-//user cookie
-
-if (typeof Cookies.get('NamingGameUser') === 'undefined'){
-    var user_str = uuid();
-    Cookies.set('NamingGameUser',
-        user_str
-        );
-
+    //user cookie
+    if (typeof Cookies.get('NamingGameUser') === 'undefined') {
+        var user_str = uuid();
+        Cookies.set('NamingGameUser', user_str );
     };
 
-$.ajax({
-                                    url: url_send_user_info+user_str,
-                                    success: function(result2){
-        
-            window.location.href = url_home;}
-
+    $.ajax({
+        url: url_send_user_info+user_str,
+        success: function (result2) {
+            window.location.href = url_home;
+        }
     });
-var user_str = Cookies.get('NamingGameUser');
- //window.location.href =  url_send_user_info+user_str;
 
-$("#username").html(user_str);
+    var user_str = Cookies.get('NamingGameUser');
+    //window.location.href =  url_send_user_info+user_str;
+    $("#username").html(user_str);
 
-    $(document).ajaxStop(function () { $("#username").html(user_str); });
-    
+    $(document).ajaxStop(function () { 
+        $("#username").html(user_str); 
+    });
 });
-
-
