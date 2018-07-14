666cdd0e772324b503f9a3d03b26bd54176ae4ff
William Schueller <william.schueller@gmail.com>
2018-07-15 01:30:40 +0200

    travis test import

--- update_ngal.sh
+++ update_ngal.sh
@@ -11,7 +11,6 @@ then
 else
 	cd ../ng_userxp_deps/naminggamesal
 	git pull
-	python3 -c "import naminggamesal" || git checkout develop && python3 setup.py develop
 	cd /code
 fi;
 touch ng/models.py
