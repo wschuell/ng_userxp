cf22f1e605121e336d046457ab09b1c0ab9ab226
William Schueller <william.schueller@gmail.com>
2018-07-19 14:27:10 +0200

    postgres as docker

---
+++ init_db/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE ng_userxp;
+CREATE USER ng_userxp_user WITH PASSWORD 'pass';
+CREATE DATABASE naminggames;
+CREATE USER naminggames WITH PASSWORD 'naminggames';
+ALTER USER ng_userxp_user CREATEDB;
