8f85e223385becbfadfaf54b2c05d28360fa5b8c
William Schueller <william.schueller@gmail.com>
2018-08-04 22:03:51 +0200

    finished backupp

--- backup_sql.sh
+++ backup_sql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-docker exec --user postgres -t ng pg_dumpall -c | gzip > ./backup_postgres/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql.gz
+mkdir -p backup_postgres
+docker exec --user postgres -t nguserxp_db_1 pg_dumpall -c | gzip > ./backup_postgres/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql.gz
