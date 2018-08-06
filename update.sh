6230b31ab5367a7e363a91a2a26e443142234525
William Schueller <william.schueller@gmail.com>
2018-08-06 09:19:04 +0200

    changed update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 git pull
-python manage.py makemigrations ng
-python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py makemigrations &&
+python manage.py makemigrations auth &&
+python manage.py makemigrations ng &&
+python manage.py migrate auth &&
+python manage.py migrate &&
+python manage.py migrate ng &&
+python manage.py collectstatic --noinput &&
+
 touch main_site/wsgi.py
