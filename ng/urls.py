e7619f5fde600f91e689e4bd5aab504d8791568b
Sandy Manolios <sandy.manolios@laposte.net>
2018-08-30 17:30:45 +0200

    Ménage URLS

--- ng/urls.py
+++ ng/urls.py
@@ -18,13 +18,12 @@ urlpatterns = [
     #url(r'^choose_experiment/$', views.choose_experiment, name='choose_experiment'),
     url(r'^new_experiment/(?P<xp_cfg_name>[0-9a-z]+)$', views.new_experiment, name='new_experiment_cfg'),
     #url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', views.exp_resume, name='exp_resume'),
-    url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/results_srtheo/$', views.result_srtheo, name='results_srtheo'),#ResultsView.as_view(), name='results'),
+    #url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', views.exp_resume, name='exp_resume'),
+    ##  url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/results_srtheo/$', views.result_srtheo, name='results_srtheo'),#ResultsView.as_view(), name='results'),
     url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})-hearer-(?P<meaning>[0-9a-z]+)/results/$', views.result_hearer, name='results_hearer'),#ResultsView.as_view(), name='results'),
      url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})-speaker-(?P<meaning>[0-9a-z]+)-(?P<word>[0-9a-z]+)/results/$', views.result_speaker, name='results_speaker'),#ResultsView.as_view(), name='results'),
     url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})-hearer-$', views.result_hearer, name='results_hearer_base'),#ResultsView.as_view(), name='results'),
      url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})-speaker-$', views.result_speaker, name='results_speaker_base'),#ResultsView.as_view(), name='results'),
-    url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/vote/$', views.vote, name='vote'),
     url(r'^(?P<xp_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/continue/$', views.continue_userxp, name='continue'),
     #url(r'^accounts/login/$', django.contrib.auth.views.login,name='login_old'),
     url(r'^logout/$', django.contrib.auth.views.logout, {'next_page': '/login'}, name='logout'),
@@ -45,12 +44,12 @@ urlpatterns = [
 
     url(r'^$',views.home,name='home'),
 
-    url(r'^test/$',views.test,name='test'),
-    url(r'^accounts/profile/$', views.new_experiment, name='new_experiment_2'),
+    ## url(r'^test/$',views.test,name='test'),
+    #url(r'^accounts/profile/$', views.new_experiment, name='new_experiment_2'),
     #url(r'^accounts/login/$', auth_views.LoginView.as_view()),
 
-    url(r'^userinfo_$', views.none, name='send_user_info_base'),
-    url(r'^userinfo_(?P<username>[\w-]+)$', views.create_and_login, name='send_user_info'),
+    ## url(r'^userinfo_$', views.none, name='send_user_info_base'),
+    ## url(r'^userinfo_(?P<username>[\w-]+)$', views.create_and_login, name='send_user_info'),
 
 	######
 	url(r'^story$', views.story, name='story'),
--- ng/views.py
+++ ng/views.py
@@ -156,26 +156,6 @@ class ResultsView(LoginRequiredMixin, generic.DetailView):
 
 
 
-@csrf_protect
-@login_required(login_url='/login/')
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'ng/detail.html', {
-            'question': question,
-            'use_matomo': settings.MATOMO_USAGE,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('ng:results', args=(question.id,)))
 
 
 @login_required(login_url='/login/')
